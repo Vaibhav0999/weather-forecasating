79cca6a72a10b6175f0e2f48a1bebc4d7e1263d1
Vaibhav0999 <vaibhav.nment@gmail.com>
2022-04-28 13:06:01 +0530

    first

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"vs19990917@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
